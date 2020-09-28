f056dd3e57b63c9f29c99796df72fc2f48fe6728
HaixiaBian <62028263+HaixiaBian@users.noreply.github.com>
2020-09-28 16:00:30 -0400

    Confidence blocks added

---
+++ my/bbs.js
@@ -0,0 +1,62 @@
+[{
+  "type": "bb1",
+  "message0": " %1 %2",
+  "args0": [
+    {
+      "type": "field_dropdown",
+      "name": "NAME",
+      "options": [
+        [
+          "option",
+          "OPTIONNAME"
+        ],
+        [
+          "option",
+          "OPTIONNAME"
+        ],
+        [
+          "option",
+          "OPTIONNAME"
+        ]
+      ]
+    },
+    {
+      "type": "input_value",
+      "name": "NAME"
+    }
+  ],
+  "colour": 230,
+  "tooltip": "",
+  "helpUrl": ""
+},
+{
+  "type": "bb2",
+  "message0": " %1 %2",
+  "args0": [
+    {
+      "type": "field_dropdown",
+      "name": "NAME",
+      "options": [
+        [
+          "option",
+          "OPTIONNAME"
+        ],
+        [
+          "option",
+          "OPTIONNAME"
+        ],
+        [
+          "option",
+          "OPTIONNAME"
+        ]
+      ]
+    },
+    {
+      "type": "input_value",
+      "name": "NAME"
+    }
+  ],
+  "colour": 230,
+  "tooltip": "",
+  "helpUrl": ""
+}]
